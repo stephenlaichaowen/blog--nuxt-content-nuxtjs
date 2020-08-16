729caf1c7881486cd0ac12e113efde3f97df9ff4
stephenlai <stephenlai2015@gmail.com>
2020-08-16 22:53:31 +0800

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel:"stylesheet", href:"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==", crossorigin: "anonymous" }
     ]
   },
   
